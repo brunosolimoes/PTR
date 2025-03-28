c57d03a0dcf497ebc5e083edeade107e1fdb55e7
Bruno <77175257+brunosolimoes@users.noreply.github.com>
2025-03-28 11:18:53 -0400

    Definindo assinatura das funções matrix.h

--- matrix.h
+++ matrix.h
@@ -1,1 +1,28 @@
+/*
+  Autor: Bruno de Moura Solimões - 22051316
+  dataInicio:28/03/2025
+  dataFim:
+*/
 
+define typedef matrix Matrix;
+
+//Criar matriz
+Matrix createMatrix(int ordem);
+
+//Deletar matriz
+bool deletMatrix(Matrix *mat);
+
+//obter a ordem da matriz
+int ordenMatrix(Matrix *mat)
+
+//Obter o determinante da matriz
+double detMatrix(Matrix *mat);
+
+//Cria uma segunda matriz que é o inverso da recebida
+Matrix invertMatrix(Matrix *mat);
+
+//Cria uma segunda matriz que é a transposta da recebida
+Matrix transportMatrix(Matrix *mat);
+
+//Preencher a matriz por inteira com os primeiro valores recebidos de um vetor, preenche linha por linha 
+void fillMatrix(Matrix *mat, double *values)
